d74c837288d362eb217c9cd5f3d8457b7d39140e
White, Eric <ewhite@ciena.com>
2016-03-21 15:46:25 -0400

    Add custom routing for demo. Making compatible with frost-guide

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,13 +1,16 @@
 import Ember from 'ember'
 import config from './config/environment'
+import addRoute from 'frost-guide-custom-routing/utils/addRoute'
 
 var Router = Ember.Router.extend({
   location: config.locationType
 })
 
 Router.map(function () {
-  this.route('demo', { path: '/' })
-  this.route('custom-login')
+  let routerConfig = config.APP.routingConfig
+  routerConfig.forEach((item) => {
+    addRoute.call(this, item)
+  })
 })
 
 export default Router
