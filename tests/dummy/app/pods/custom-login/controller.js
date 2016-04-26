a924d7518d9e9393c9401653d9818791a9f9747a
Roxanne Panthaky <rpanthak@ciena.com>
2016-04-26 13:27:24 -0400

    update core

--- tests/dummy/app/pods/custom-login/controller.js
+++ tests/dummy/app/pods/custom-login/controller.js
@@ -1,23 +1,13 @@
 import Ember from 'ember'
-import {default as loginFormModel} from './model'
 
 const {Controller} = Ember
 
 export default Controller.extend({
-  loginFormModel,
   valid: false,
 
   actions: {
-    loginFormValueChanged (value) {
-      this.set('loginFormValue', value)
-    },
 
-    onValidationHandler (e) {
-      this.set('valid', e.valid)
-    },
-
-    submitLogin () {
-      let data = this.get('loginFormValue')
+    submitLogin (data) {
       this.notifications.addNotification({
         message: 'username: ' + data.username +
           ', password: ' + data.password + ', rememberme: ' + data.rememberMe +
@@ -26,7 +16,6 @@ export default Controller.extend({
         autoClear: true,
         clearDuration: 2000
       })
-      this.set('loginFormValue', {})
     }
   }
 })
